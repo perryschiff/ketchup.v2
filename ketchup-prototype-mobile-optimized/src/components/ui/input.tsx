c62ff1980f6a35706a3a6c59fe422321b134aa93
perryschiff <perryschiff@gmail.com>
2025-08-11 20:07:21 -0400

    Delete ketchup-prototype-mobile-optimized directory

--- ketchup-prototype-mobile-optimized/src/components/ui/input.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { cn } from '@/lib/cn'
-export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
-  ({ className, ...props }, ref) => (
-    <input ref={ref} className={cn('border rounded-xl px-4 py-3 w-full text-base', className)} {...props} />
-  )
-)
-Input.displayName = 'Input'
